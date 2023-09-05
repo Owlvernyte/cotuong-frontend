b91c6e763092939e81fa18db249307f51f750e46
vvduong2k2 <duongnguyen2002.bmt@gmail.com>
2023-09-05 17:48:48 +0700

    feat(tournament): fix

--- app/tournament/page.tsx
+++ app/tournament/page.tsx
@@ -15,15 +15,31 @@ export default function Home() {
             Còn lại: 3 giờ 2 phút 5 giây
           </p>
         </div>
-        <div className="w-full h-[250px] flex justify-around justify-center">
-          <div className="w-[200px]  h-[220px] bg-dirt-300 rounded shadow"></div>
+        <div className="w-full h-[250px] flex justify-between items-center">
+          <div className="flex items-center justify-center w-[200px]  h-[220px] bg-dirt-300 rounded shadow mx-[15px] my-[15px]">
+            <img
+              className="w-[150px] h-[150px]"
+              src="/icons/Cup.svg"
+              alt="nyte"
+            />
+          </div>
 
-          <div className="w-96 h-44 bg-neutral-500 rounded shadow"></div>
+          <div className="grid grid-cols-3 grid-rows-3 justify-items-center w-96 h-[220px] bg-bamboo-300 text-dirt-100 rounded shadow basis-3/5">
+            <h3>Top</h3>
+            <h3>UserName</h3>
+            <h3>Win</h3>
+            <h3>1</h3>
+            <h3>Player 1</h3>
+            <h3>12</h3>
+            <h3>2</h3>
+            <h3>Player 2</h3>
+            <h3>10</h3>
+          </div>
 
-          <div className="w-64 h-32 bg-yellow-800 rounded shadow"></div>
+          <div className="w-64 h-[220px] bg-dirt-300 rounded shadow mx-[15px] my-[15px]"></div>
         </div>
       </div>
-      <div className="w-[1400px] h-[500px] bg-bamboo-400 rounded shadow"></div>
+      <div className="w-[1400px] h-[500px] bg-bamboo-400 rounded shadow "></div>
     </div>
   );
 }
