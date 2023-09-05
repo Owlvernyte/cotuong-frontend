72c29cc13748cd168025013cf43545ea6054407c
Dat Hoang <dat09t@gmail.com>
2023-09-05 22:19:15 +0700

    Squashed commit of the following:
    commit 7cbc967ac49cae955f64fb517fac6e2b2f8c420c
    Author: Dat Hoang <dat09t@gmail.com>
    Date:   Tue Sep 5 22:18:38 2023 +0700
    
        don rac so

--- app/(basic)/layout.tsx
+++ app/(basic)/layout.tsx
@@ -1,13 +1,17 @@
-import Header from '@/components/Header'
+import Footer from '@/components/layouts/Footer'
+import Header from '@/components/layouts/Header'
 import React from 'react'
 
 function BasicLayout({ children }: React.PropsWithChildren) {
     return (
         <div className="app">
             <div className="app-page">
-                <div className='flex flex-col h-full'>
+                <div className="flex flex-col h-full">
                     <Header />
-                    <main className='p-2 flex-1'>{children}</main>
+                    <main className="p-2 flex-1 overflow-y-auto">
+                        {children}
+                    </main>
+                    <Footer/>
                 </div>
             </div>
         </div>
