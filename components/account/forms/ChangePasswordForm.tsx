d0199ab1be97ead9bccaeccbc743172f82faa6bb
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-10-10 15:45:36 +0700

    refactor(account): update ChangePasswordForm

--- components/account/forms/ChangePasswordForm.tsx
+++ components/account/forms/ChangePasswordForm.tsx
@@ -4,33 +4,35 @@ import React from "react"
 
 const ChangePasswordForm: React.FC = () => {
   return (
-    <div className="p-2 flex flex-col">
-      <div className="text-bamboo-100  text-left text-sm sm:text-base md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl font-normal pt-4 ">
+    <div className="flex flex-col py-2">
+      <div className="text-bamboo-100 text-left text-sm md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl font-normal">
         Mật Khẩu
       </div>
-      <div className="p-2 items-center inline-flex flex-col pt-7 gap-8 w-full">
-        <input
-          type="text"
-          name="password"
-          placeholder="Mật khẩu hiện tại"
-          className="input input-bordered input-lg w-full bg-bamboo-100 placeholder-dirt-400 placeholder-opacity-50 text-xl"
-        />
-        <input
-          type="text"
-          name="n_password"
-          placeholder="Mật khẩu mới"
-          className="input input-bordered input-lg w-full bg-bamboo-100 placeholder-dirt-400 placeholder-opacity-50 text-xl"
-        />
-        <input
-          type="text"
-          name="rn_password"
-          placeholder="Xác nhận mật khẩu mới"
-          className="input input-bordered input-lg w-full bg-bamboo-100 placeholder-dirt-400 placeholder-opacity-50 text-xl"
-        />
-        <button className="btn btn-secondary btn-lg w-full">
-          Cập Nhật Mật Khẩu{" "}
-        </button>
-      </div>
+      <form>
+        <div className="items-center inline-flex flex-col pt-4 gap-4 w-full">
+          <input
+            type="password"
+            name="currentPassword"
+            placeholder="Mật khẩu hiện tại"
+            className="input input-bordered input-lg w-full bg-bamboo-100 placeholder-dirt-400 placeholder-opacity-50 text-xl"
+          />
+          <input
+            type="password"
+            name="newPassword"
+            placeholder="Mật khẩu mới"
+            className="input input-bordered input-lg w-full bg-bamboo-100 placeholder-dirt-400 placeholder-opacity-50 text-xl"
+          />
+          <input
+            type="password"
+            name="confirmNewPassword"
+            placeholder="Xác nhận mật khẩu mới"
+            className="input input-bordered input-lg w-full bg-bamboo-100 placeholder-dirt-400 placeholder-opacity-50 text-xl"
+          />
+          <button type="submit" className="btn btn-secondary btn-lg w-full">
+            Cập Nhật Mật Khẩu
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
