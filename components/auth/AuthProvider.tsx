3b9a783136e6cc376764c897565ee137d1b7b9f6
Dat Hoang <fiezt@outlook.com>
2023-10-15 14:51:15 +0700

    updated auth logic

--- components/auth/AuthProvider.tsx
+++ components/auth/AuthProvider.tsx
@@ -5,14 +5,17 @@ import { useStore } from '@/lib/zustand/store'
 import { useRouter, usePathname } from 'next/navigation'
 import React, { PropsWithChildren } from 'react'
 
+const authPaths = ['/signin', '/signup']
+const publicPaths = ['/', '/contact']
+
 function AuthProvider({ children }: PropsWithChildren) {
     const { data, isLoading } = useUser()
     const { authorized, authorize, unauthorize, setUser, eraseUser } =
         useStore()
     const router = useRouter()
     const pathname = usePathname()
-    const inAuthPath =
-        pathname.startsWith('/signin') || pathname.startsWith('/signup')
+    const inAuthPath = authPaths.includes(pathname)
+    const inPublicPath = publicPaths.includes(pathname)
 
     React.useEffect(() => {
         if (data) {
@@ -22,9 +25,13 @@ function AuthProvider({ children }: PropsWithChildren) {
             unauthorize()
             eraseUser()
         }
-    }, [authorize, data, unauthorize])
+    }, [authorize, data, eraseUser, setUser, unauthorize])
 
     React.useEffect(() => {
+        if (inPublicPath) {
+            return
+        }
+
         if (authorized) {
             if (inAuthPath) {
                 router.push('/')
@@ -32,8 +39,12 @@ function AuthProvider({ children }: PropsWithChildren) {
             }
             return
         }
+
+        if (inAuthPath) {
+            return
+        }
         router.replace('/signin')
-    }, [authorized, router, pathname, inAuthPath])
+    }, [authorized, router, pathname, inAuthPath, inPublicPath])
 
     return (
         <>
