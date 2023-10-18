36879aab9c0aaacbef1b6452c54f8e25801b7a69
phamtruong7302 <truongpham7302@gmail.com>
2023-10-19 00:35:38 +0700

    feat(piece): add canon logic

--- lib/game/QuanCo/collections/Cannon.ts
+++ lib/game/QuanCo/collections/Cannon.ts
@@ -1,3 +1,4 @@
+import Board from '../../Board'
 import Piece, { IPiece, PieceType } from '../Piece'
 
 class Cannon extends Piece {
@@ -5,6 +6,92 @@ class Cannon extends Piece {
         super(options)
         this.pieceType = PieceType.Cannon
         this.signature = 'C'
+    }
+    CheckRow(destinationCoordinate: ICoordination, board: Board): boolean {
+        if (this.coord.x < destinationCoordinate.x) {
+            for (let i = this.coord.x + 1; i < destinationCoordinate.x; i++) {
+                if (board.squares[i][destinationCoordinate.y] !== null)
+                    return false;
+            }
+            return true;
+        }
+        for (let i = this.coord.x - 1; i >= destinationCoordinate.x; i--) {
+            if (board.squares[i][destinationCoordinate.y] !== null)
+                return false;
+        }
+        return true;
+    }
+
+    CheckColumns(destinationCoordinate: ICoordination, board: Board): boolean {
+        if (this.coord.y < destinationCoordinate.y) {
+            for (let i = this.coord.y + 1; i <= destinationCoordinate.y; i++) {
+                if (board.squares[destinationCoordinate.x][i] !== null)
+                    return false;
+            }
+            return true;
+        }
+        for (let i = this.coord.y - 1; i >= destinationCoordinate.y; i--) {
+            if (board.squares[destinationCoordinate.x][i] !==null)
+                return false;
+        }
+        return true;
+    }
+    CheckRemoveDesRow(destinationCoordinate: ICoordination, board: Board): boolean {
+        let count = 0;
+        if (this.coord!.x < destinationCoordinate.x) {
+            for (let i = this.coord!.x + 1; i < destinationCoordinate.x; i++) {
+                if (board.squares[i][destinationCoordinate.y] !== null)
+                    count++;
+            }
+        }
+        else {
+            for (let i = this.coord!.x - 1; i > destinationCoordinate.x; i--) {
+                if (board.squares[i][destinationCoordinate.y] !== null)
+                    count++;
+            }
+        }
+        if(count == 1)
+            return true;
+        return false;
+    }
+
+    CheckRemoveDesColumns(destinationCoordinate: ICoordination, board: Board): boolean {
+        let count = 0;
+        if (this.coord!.y < destinationCoordinate.y) {
+            for (let i = this.coord!.y + 1; i < destinationCoordinate.y; i++) {
+                if (board.squares[this.coord!.x][i] !== null)
+                    count++;
+            }
+        }
+        else {
+            for (let i = this.coord!.y - 1; i > destinationCoordinate.y; i--) {
+                if (board.squares[destinationCoordinate.x][i] !== null)
+                    count++;
+            }
+        }
+        if(count == 1)
+            return true;
+        return false;
+    }
+    override isValidMove(destination: ICoordination, board: Board): Boolean {
+        const isBaseValidMove = super.isValidMove(destination, board);
+        if (!isBaseValidMove)
+            return false;
+        if(destination.x != this.coord.x && destination.y != this.coord.y)
+            return false;
+        if (!board.squares[destination.x][destination.y]) {
+            if (destination.x != this.coord.x)
+                return this.CheckRow(destination, board);
+            if (destination.y != this.coord!.y)
+                return this.CheckColumns(destination, board);
+        }
+           else {
+            if (destination.x !== this.coord!.x)
+                return this.CheckRemoveDesRow(destination, board);
+            if (destination.y !== this.coord!.y)
+                return this.CheckRemoveDesColumns(destination, board);
+        }
+        return false;
     }
 }
 
