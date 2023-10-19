431da416adcd7e2244bb5319ad15abb0eb2740f1
Dat Hoang <fiezt@outlook.com>
2023-10-19 17:48:00 +0700

    updated soldier logic

--- lib/game/QuanCo/collections/Soldier.ts
+++ lib/game/QuanCo/collections/Soldier.ts
@@ -1,35 +1,50 @@
-import Board from "../../Board";
-import Piece, { IPiece, PieceType } from "../Piece";
+import Board from '../../Board'
+import Piece, { IPiece, PieceType } from '../Piece'
 
 class Soldier extends Piece {
-  constructor(options?: IPiece) {
-    super(options);
-    this.pieceType = PieceType.Soldier;
-    this.signature = "P";
-  }
-  override isValidMove(destination: ICoordination, board: Board): Boolean {
-    const isBaseValidMove = super.isValidMove(destination, board);
-    if (!isBaseValidMove) return false;
-
-    const moveX = Math.abs(this.coord.x - destination.x);
-    const moveY = Math.abs(this.coord.y - destination.y);
-
-    // Cho phép đi từng ô một ngang hoặc dọc
-    if (moveX + moveY != 1) {
-      return false;
+    constructor(options?: IPiece) {
+        super(options)
+        this.pieceType = PieceType.Soldier
+        this.signature = 'P'
     }
-    // Ngăn không cho con tốt đi lùi
-    if (
-      (this.isRed && this.coord.x < destination.x) ||
-      (!this.isRed && this.coord.x > destination.x)
-    ) {
-      return false;
+    override isValidMove(destination: ICoordination, board: Board): Boolean {
+        const isBaseValidMove = super.isValidMove(destination, board)
+        if (!isBaseValidMove) return false
+
+        const moveX = Math.abs(this.coord.x - destination.x)
+        const moveY = Math.abs(this.coord.y - destination.y)
+
+        // Cho phép đi từng ô một ngang hoặc dọc
+        if (moveX + moveY != 1) {
+            return false
+        }
+
+        // Tot do
+        if (
+            this.isRed &&
+            // Ngăn không cho con tốt đi lùi
+            (this.coord.x < destination.x ||
+                // Ngăn không di ngang khi chua qua song
+                (this.coord.x >= 5 && moveY != 0))
+        ) {
+            return false
+        }
+
+        // Tot xanh/den
+        if (
+            !this.isRed &&
+            // Ngăn không cho con tốt đi lùi
+            (this.coord.x > destination.x ||
+                // Ngăn không di ngang khi chua qua song
+                (this.coord.x <= 4 && moveY != 0))
+        ) {
+            return false
+        }
+
+        // TODO: Sẽ cập nhật lại khi có Xoay Bàn Cờ!
+
+        return true
     }
-
-    // TODO: Sẽ cập nhật lại khi có Xoay Bàn Cờ!
-
-    return true;
-  }
 }
 
-export default Soldier;
+export default Soldier
