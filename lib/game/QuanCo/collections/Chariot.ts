17b1e94f2e666c40dcebc5db889a087c81cd7309
phamtruong7302 <truongpham7302@gmail.com>
2023-10-19 15:21:18 +0700

    feat(piece): add Chariot logic

--- lib/game/QuanCo/collections/Chariot.ts
+++ lib/game/QuanCo/collections/Chariot.ts
@@ -7,9 +7,43 @@ class Chariot extends Piece {
         this.pieceType = PieceType.Chariot
         this.signature = 'R'
     }
-    // override isValidMove(destination: ICoordination, board: Board): Boolean {
-    //     return true
-    // }
+    override isValidMove(destination: ICoordination, board: Board): Boolean {
+        const isBaseValidMove = super.isValidMove(destination, board)
+        if (!isBaseValidMove) return false
+
+        const directionX = destination.x - this.coord.x
+        const directionY = destination.y - this.coord.y
+        const moveX = Math.abs(directionX)
+        const moveY = Math.abs(directionY)
+
+        // Khong cho phep di cheo
+        if (moveX > 0 && moveY > 0) return false
+
+        const isOverPiece = board.squares[destination.x][destination.y] != null
+
+        // Kiem tra cot
+        if (moveY == 0) {
+            const headIndex = directionX > 0 ? this.coord.x : destination.x
+            const tailIndex = directionX < 0 ? this.coord.x : destination.x
+            for (let i = headIndex + 1; i < tailIndex; i++) {
+                if (board.squares[i][this.coord.y] !== null) 
+                    return false;
+            }
+        }
+
+        // Kiem tra dong
+        if (moveX == 0) {
+            const headIndex = directionY > 0 ? this.coord.y : destination.y
+            const tailIndex = directionY < 0 ? this.coord.y : destination.y
+            for (let j = headIndex + 1; j < tailIndex; j++) {
+                if (board.squares[this.coord.x][j] !== null) 
+                    return false;
+            }
+        }
+
+        // TODO: Sẽ cập nhật lại khi có Xoay Bàn Cờ!
+        return true
+    }
 }
 
 export default Chariot
