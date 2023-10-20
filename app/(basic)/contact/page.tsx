0322332fcd9a21c98cf3501536425924489fce5f
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-10-20 21:03:59 +0700

    feat(contact): Add card and title for page

--- app/(basic)/contact/page.tsx
+++ app/(basic)/contact/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import Title from "@/components/ui/Title"
+import React from "react"
 
 function Contact() {
   return (
-    <div>Contact & Help</div>
+    <div
+      id="contact-and-help"
+      className="grid grid-cols-8 gap-2 grid-flow-row-dense flex-1 h-full"
+    >
+      <div
+        id="contact"
+        className="card bg-bamboo-400 rounded-md w-full col-span-4 overflow-y-auto max-h-full"
+      >
+        <div className="card-body">
+          <Title text="LIÊN HỆ & GÓP Ý" textColor="text-bamboo-100" />
+          <p className="text-bamboo-100 text-center text-2xl text-opacity-50 py-2">
+            Để lại lời nhắn trực tiếp cho chúng tôi tại trang web
+          </p>
+
+          {/* Form */}
+          {/* Hoặc */}
+        </div>
+      </div>
+
+      <div
+        id="help"
+        className="card bg-bamboo-400 rounded-md w-full col-span-4 overflow-y-auto max-h-full"
+      >
+        <div className="card-body">
+          <Title text="CÂU HỎI THƯỜNG GẶP" textColor="text-bamboo-100" />
+          {/* List */}
+        </div>
+      </div>
+    </div>
   )
 }
 
