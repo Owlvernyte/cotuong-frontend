7e42998fad2979d010481145b6a3705b86a77799
vvduong2k2 <duongnguyen2002.bmt@gmail.com>
2023-10-21 00:16:37 +0700

    update: UI

--- app/(basic)/matches/page.tsx
+++ app/(basic)/matches/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import MatchesTable from "@/components/matches/MatchesTable";
 function Matches() {
   return (
-    <div>Matches</div>
-  )
+    <div className="flex flex-col h-full justify-center items-center w-full py-4">
+      <h1 className="text-5xl py-8">Lịch Sử Đấu</h1>
+      <MatchesTable />
+    </div>
+  );
 }
 
-export default Matches
+export default Matches;
