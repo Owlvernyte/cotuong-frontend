f1141f981e85fd28a197bedb89073c0c456a7500
Dat Hoang <fiezt@outlook.com>
2023-10-23 13:22:52 +0700

    added signalr eventname enum

--- lib/types/game.d.ts
+++ lib/types/game.d.ts
@@ -4,3 +4,17 @@ interface ICoordination {
 }
 
 type CoordinationType = ICoordination
+
+enum SignalREvent {
+    LoadBoard = 'LoadBoard',
+    Moved = 'Moved',
+    MoveFailed = 'MoveFailed',
+    Ended = 'Ended',
+    Chatted = 'Chatted',
+    Joined = 'Joined',
+    Left = 'Left',
+    HostLeft = 'HostLeft',
+    RoomDeleted = 'RoomDeleted',
+    Connected = "connected",
+    Error = "error"
+}
